ba7f8317138fea641230a41c77cb448365aad1fe
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-01-22 18:39:02 -0700

    something

---
+++ FoodItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mission_3
+{
+    internal class FoodItem
+    {
+        public string foodName = "";
+        public string foodCategory = "";
+        public int foodQuantity = 0;
+        public DateTime foodExpDate = DateTime.MinValue;
+
+    }
+
+ 
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using mission_3;
+List<FoodItem> foodItemList = new List<FoodItem>();
+string answer = "";
+
+Console.WriteLine("Would you like to add a new food item? ");
+answer = Console.ReadLine().ToLower();
+if (answer == "yes")
+{
+    FoodItem fi = new FoodItem();    
+    Console.WriteLine("What is the name of the food item? ");
+    fi.foodName = Console.ReadLine();
+    Console.WriteLine("What is the Category ex. 'Dairy','Poultry', 'Desserts' etc.");
+    fi.foodCategory = Console.ReadLine();
+    Console.WriteLine("What is the Quantity? ");
+    fi.foodQuantity = Int32.Parse(Console.ReadLine());
+    Console.WriteLine("What is the Expiration Date (in format of 'MM/DD/YYYY'? ");
+    fi.foodExpDate = DateTime.Parse(Console.ReadLine());
+
+    foodItemList.Add(fi);
+
+    Console.WriteLine("Here is the current list: ");
+    
+    for (int i = 0; i < foodItemList.Count; i++)
+    {
+        Console.WriteLine($"{foodItemList[i].foodName} \n{foodItemList[i].foodCategory} \n{foodItemList[i].foodQuantity} \n{foodItemList[i].foodExpDate}");
+
+    }
+
+    Console.WriteLine("Would you like to add another, delete, edit ")
+
+
+}
+
