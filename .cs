f8a7ee55a2d3cf5cd793cbb54346b2c8bcbb7466
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-01-24 14:37:34 -0700

    finished the hard coding need to do exception handling

---
+++ .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mission_3
+{
+
+    internal class addFoodItem
+    {
+        public addFoodItem()
+        {
+
+        }
+
+    }
+}
